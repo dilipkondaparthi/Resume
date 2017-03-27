99293a8f4166d684ae89f032c7bf84629a02dcd8
dilipkondaparthi <es12b1010@iith.ac.in>
2017-03-28 03:49:45 +0530

    Adding Resume Folder
    Adding Resume Folder

---
+++ js/resumeBuilder.js
@@ -0,0 +1,391 @@
+var bio = {
+ "name": "DILIP KONDAPARTHI",
+ "role": "Software Developer",
+ "contact": {
+  "linkedin": "dilipkondaparthi",
+  "email": "dilipkondaparthi@gmail.com",
+  "github": "@dilipkondaparthi",
+  "facebook": "dilip.kondaparthi",
+  "location": "Madhapur, Hyderabad, Telangana"
+ },
+ "welcomeMessage": "I'm currently working as a Software Developer in VirtusaPolaris. Thanks for stopping by!",
+ "skills": ["C++", "C", "MySQL", "HTML", "CSS", "JavaScript"],
+ "biopic": "images/dil.jpg"
+};
+
+var work = {
+ "jobs": [{
+  "employer": "VirtusaPolaris",
+  "url": "http://www.virtusapolaris.com/",
+  "title": "Software Developer",
+  "dates": "September 2016 - Present",
+  "location": "Nanakram Guda, Hyderabad, Telangana",
+  "description": "<u>Project Description</u> - Developed a template for bank to check the loan eligibility of a customer by using customer’s CIBIL score and write their eligibility status on the blocks of blockchain network. <br><u>Block Chain Training</u> – Gone through a 4 week training on blockchain(which includes study on Introduction to cryptography and cryptocurrencies, Decentralization of Bitcoin, Mechanics of Bitcoin, Storage and Usage of Bitcoins, Bitcoin Mining and Mining puzzles, Anonymity of Bitcoin, Bitcoin as a platform, Community Politics and regulations in Bitcoin)."
+ }]
+};
+
+var responsibilities = {
+ "details": [{
+  "description": "Campus Publicity Co-ordinator at ELAN(Techno Cultural Fest) of IIT Hyderabad."
+ }, {
+  "description": "Co-ordinator at Photography club, IIT Hyderabad."
+ }, {
+  "description": "NSO-Hockey player at IIT Hyderabad."
+ }]
+};
+
+
+var education = {
+ "schools": [{
+  "name": "INDIAN INSTITUTE OF TECHNOLOGY, HYDERABAD",
+  "degree": "B.Tech in Computer Science and Engineering Science: GPA: 7/10.",
+  "dates": "2012 - 2016",
+  "location": "Sangareddy, Kandi, Telangana",
+  "major": "Computer Science",
+  "url": "http://www.iith.ac.in/",
+  "courses": "Data Structures and Algorithms, Parallel and Concurrent Programming, Design and Analysis of Algorithms, Operating Systems, Databases, Theory of Computation, Engineering Graphics."
+ }]
+};
+
+
+var onCourse = {
+ "onlineCourses": [{
+  "title": "Bitcoin and Cryptocurrency Technologies",
+  "url": "https://www.coursera.org/learn/cryptocurrency",
+  "school": "Coursera",
+  "dates": "December 2016"
+ }, {
+  "title": "IBM Blockchain for developers",
+  "url": "https://developer.ibm.com/courses/all-courses/blockchain-for-developers/",
+  "school": "IBM",
+  "dates": "February 2017"
+ }, {
+  "title": "IITBombayX: CS213.2x Implementation of Data Structures",
+  "url": "https://courses.edx.org/courses/course-v1%3AIITBombayX%2BCS213.2x%2B1T2016/",
+  "school": "edX",
+  "dates": "July 2016 - September 2016"
+ }, {
+  "title": "Microsoft: DAT204x Introduction to R for Data Science",
+  "url": "https://courses.edx.org/courses/course-v1:Microsoft+DAT204x+5T2016/",
+  "school": "edX",
+  "dates": "September 2016"
+ }, {
+  "title": "Intro to HTML and CSS",
+  "url": "https://www.udacity.com/course/intro-to-html-and-css--ud304",
+  "school": "Udacity",
+  "dates": "February 2017"
+ }, {
+  "title": "Responsive Web Design Fundamentals by  Google",
+  "url": "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893",
+  "school": "Udacity",
+  "dates": "February 2017"
+ }, {
+  "title": "JavaScript Basics",
+  "url": "https://www.udacity.com/course/javascript-basics--ud804",
+  "school": "Udacity",
+  "dates": "February 2017"
+ }, {
+  "title": "Intro to iOS App Development with Swift",
+  "url": "https://www.udacity.com/course/intro-to-ios-app-development-with-swift--ud585",
+  "school": "Udacity",
+  "dates": "February 2017"
+ }, {
+  "title": "HTML & CSS",
+  "url": "https://www.codecademy.com/learn/web",
+  "school": "codecademy",
+  "dates": "May 2015"
+ }, {
+  "title": "JavaScript",
+  "url": "https://www.codecademy.com/learn/javascript",
+  "school": "codecademy",
+  "dates": "May 2015"
+ }, {
+  "title": "PHP",
+  "url": "https://www.codecademy.com/learn/php",
+  "school": "codecademy",
+  "dates": "May 2015"
+ }, {
+  "title": "Learn SQL",
+  "url": "https://www.codecademy.com/learn/learn-sql",
+  "school": "codecademy",
+  "dates": "May 2015"
+ }, {
+  "title": "SQL: Table Transformation",
+  "url": "https://www.codecademy.com/learn/sql-table-transformation",
+  "school": "codecademy",
+  "dates": "May 2015"
+ }, {
+  "title": "SQL: Analyzing Business Metrics",
+  "url": "https://www.codecademy.com/learn/sql-analyzing-business-metrics",
+  "school": "codecademy",
+  "dates": "May 2015"
+ }]
+};
+
+
+
+var projects = {
+ "projects": [{
+  "title": "Implementation of Szymański's algorithm and Eisenberg & McGuire algorithm",
+  "description": "Main insight of this project is to implement new locks - And i also developed an application to test these performance of the algorithms which is implemented in C++.",
+  "dates": "August 2015 - December 2015",
+  //"images": []
+ }, {
+  "title": "Energy Monitoring Website",
+  "description": "Designed a website which displays graph of the Instantaneous Power of IITH for the present day and also Total Power values of past days and months.",
+  "dates": "January 2016 - May 2016",
+  //"images": []
+ }, {
+  "title": "Cab Sharing Website",
+  "description": "Wrote PHP Code in the implementation of Cab Sharing Website for Creating & Viewing rides(posts).",
+  "dates": "January 2016 - May 2016",
+  //"images": []
+ }, {
+  "title": "PitchPerfect: My First iPhone App",
+  "description": "An iPhone app that records a conversation between you and a friend, and then makes your voices sound like a Chipmunk or Darth Vader.",
+  "dates": "January 2017 - February 2017",
+  //"images": []
+ }, {
+  "title": "Robotics Line Follower",
+  "description": "Implemented a machine that follows a black line on white surface using light sensors namely Light dependent Resistors (LDRs) to detect black line on white surface.",
+  "dates": "January 2013 - May 2013",
+  //	"images": []
+ }]
+};
+
+var acadAcheive = {
+ "achieve": [{
+  "details": "98 in IIT-JEE 2012 (out of 500000 candidates)."
+ }, {
+  "details": "98.3 in AIEEE 2012 (of around 1200000 candidates)."
+ }, {
+  "details": "98 in EAMCET 2012 (of around 130000 candidates)."
+ }, {
+  "details": "95.1 in IPE(class 12) - State Board."
+ }, {
+  "details": "89.6 in Matriculation(class 10) - SSC Board."
+ }]
+};
+
+
+var acadCompetitive = {
+ "achieve": [{
+  "rank": "1st",
+  "link": "https://www.hackerearth.com/challenge/college/indiahacks-codehunt-2q/leaderboard/",
+  "contest": "IndiaHacks - CodeHunt 2Q"
+ }, {
+  "rank": "2nd",
+  "link": "https://www.hackerearth.com/challenge/college/coders-g/leaderboard/",
+  "contest": "CODERS - G"
+ }, {
+  "rank": "2nd",
+  "link": "https://www.hackerearth.com/challenge/college/india-hacks-new-year-treat-code-hack/leaderboard/",
+  "contest": "IndiaHacks New Year Treat ( Code-Hack )"
+ }, {
+  "rank": "2nd",
+  "link": "https://www.hackerearth.com/challenge/college/indiahacks-code-mania-7/leaderboard/",
+  "contest": "IndiaHacks- CODE-MANIA 7"
+ }, {
+  "rank": "3rd",
+  "link": "https://www.hackerearth.com/challenge/college/code-hack-christmas-feast/leaderboard/",
+  "contest": "IndiaHacks Code-Hack Christmas Feast"
+ }, {
+  "rank": "5th",
+  "link": "https://www.hackerearth.com/challenge/college/indiahacks-codehunt-2p/leaderboard/",
+  "contest": "IndiaHacks - CodeHunt 2P"
+ }, {
+  "rank": "8th",
+  "link": "https://www.hackerearth.com/challenge/college/coders-f-1/leaderboard/",
+  "contest": "CODERS - F"
+ }]
+};
+
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedcontact = [];
+var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contact.linkedin);
+var formattedfacebook = HTMLfacebook.replace("%data%", bio.contact.facebook);
+var formattedImage = HTMLbioPic.replace("%data%", bio.biopic);
+var formattedemail = HTMLemail.replace("%data%", bio.contact.email);
+var formattedgithub = HTMLgithub.replace("%data%", bio.contact.github);
+var formattedlocation = HTMLlocation.replace("%data%", bio.contact.location);
+formattedcontact.push(formattedemail);
+formattedcontact.push(formattedgithub);
+formattedcontact.push(formattedLinkedin);
+formattedcontact.push(formattedfacebook);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedImage);
+$("#header").append(formattedMsg);
+
+if (bio.skills.length > 0) {
+ $("#header").append(HTMLskillsStart);
+ for (i in bio.skills) {
+  var formattedskills = HTMLskills.replace("%data%", bio.skills[i]);
+  $("#skills").append(formattedskills);
+ }
+}
+
+for (i in formattedcontact) {
+ $("#topContacts").append(formattedcontact[i]);
+ $("#footerContacts").append(formattedcontact[i]);
+}
+
+
+
+
+function inName(x) {
+ var d = x.trim().split(" ");
+ console.log(x);
+ return x[0].slice(0, 1).toUppercase() + x[0].slice(1).toLowerCase() + " " + x[1].toUppercase();
+}
+
+$('#main').append(internationalizeButton);
+
+
+education.display = function() {
+ if (education.schools.length > 0) {
+  for (i in education.schools) {
+   $("#education").append(HTMLschoolStart);
+
+   var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+   var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+   var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+   var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+   var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+   var formattedSchoolCourses = HTMLschoolCourses.replace("%data%", education.schools[i].courses);
+
+   $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+   $(".education-entry:last").append(formattedSchoolDates);
+   $(".education-entry:last").append(formattedSchoolLocation);
+   $(".education-entry:last").append(formattedSchoolMajor);
+   $(".education-entry:last").append(formattedSchoolCourses);
+   $(".education-entry:last").append("<br><br>");
+  }
+ }
+}
+
+education.display();
+
+function displayWork() {
+ if (work.jobs.length > 0) {
+  for (myjobs in work.jobs) {
+   $("#workExperience").append(HTMLworkStart);
+   var formattedurl = HTMLworkURL.replace("%link%", work.jobs[myjobs].url);
+   var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[myjobs].employer);
+   var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[myjobs].title);
+   var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[myjobs].location);
+   var formattedDates = HTMLworkDates.replace("%data%", work.jobs[myjobs].dates);
+   var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[myjobs].description);
+   var formattedEmployerTitle = formattedurl + formattedEmployer + formattedTitle;
+   $(".work-entry:last").append(formattedEmployerTitle);
+   $(".work-entry:last").append(formattedLocation);
+   $(".work-entry:last").append(formattedDates);
+   $(".work-entry:last").append(formattedDescription);
+  }
+ }
+}
+displayWork();
+
+projects.display = function() {
+ if (projects.projects.length > 0) {
+  for (i in projects.projects) {
+   $("#projects").append(HTMLprojectStart);
+
+   var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+   var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+   var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+   $(".project-entry:last").append(formattedProjectTitle);
+   $(".project-entry:last").append(formattedProjectDates);
+   $(".project-entry:last").append(formattedProjectDescription);
+
+   /*for(img in projects.projects[i].images) {
+     var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+     $(".project-entry:last").append(formattedProjectImage);
+   }*/
+  }
+ }
+}
+
+projects.display();
+
+
+var displayacadAchieve = function() {
+ if (acadAcheive.achieve.length > 0) {
+  for (i in acadAcheive.achieve) {
+   $("#acadAcademic").append(HTMLachieveCompetitiveStart);
+   var formattedDetails = HTMLachieveCompetitiveDesciption.replace("%data%", acadAcheive.achieve[i].details);
+   $(".achieveCompetitiveStart-entry:last").append(formattedDetails);
+  }
+  $(".achieveCompetitiveStart-entry:last").append("<br>");
+ }
+}
+
+displayacadAchieve();
+
+function displayacadCompetitive() {
+ if (acadCompetitive.achieve.length > 0) {
+  for (i in acadCompetitive.achieve) {
+
+   $("#acadCompetitive").append(HTMLachieveStart);
+   var formattedRank = HTMLachieveDescription_1.replace("%rank%", acadCompetitive.achieve[i].rank);
+   var formattedLink = HTMLachieveDescription_2.replace("%link%", acadCompetitive.achieve[i].link);
+   var formattedContest = HTMLachieveDescription_3.replace("%contest%", acadCompetitive.achieve[i].contest);
+   var formattedResult = formattedRank + formattedLink + formattedContest;
+   $(".acadCompetitive-entry:last").append(formattedResult);
+  }
+  $(".acadCompetitive-entry:last").append("<br>");
+ }
+}
+
+displayacadCompetitive();
+
+onCourse.display = function() {
+ if (onCourse.onlineCourses.length > 0) {
+  //  $("#onlineclasses").append(HTMLonlineClasses);
+  for (i in onCourse.onlineCourses) {
+   $("#onlineclasses").append(HTMLonlineStart);
+   var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", onCourse.onlineCourses[i].title).replace("#", onCourse.onlineCourses[i].url);
+   var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", onCourse.onlineCourses[i].school);
+   var formattedOnlineDates = HTMLonlineDates.replace("%data%", onCourse.onlineCourses[i].dates);
+   var formattedOnlineURL = HTMLonlineURL.replace("%data%", onCourse.onlineCourses[i].url).replace("#", onCourse.onlineCourses[i].url);
+
+   $(".onlineclasses-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+   $(".onlineclasses-entry:last").append(formattedOnlineDates);
+   //$(".education-entry:last").append(formattedOnlineURL);
+  }
+  $(".onlineclasses-entry:last").append("<br>");
+ }
+}
+
+onCourse.display();
+
+var displayResponsibility = function() {
+ if (responsibilities.details.length > 0) {
+  for (i in responsibilities.details) {
+   $('#responsibility').append(HTMLresponsibilities);
+   var formattedDetails = HTMLresponsibilitiesDescription.replace("%data%", responsibilities.details[i].description);
+   $('.responsibilities-entry:last').append(formattedDetails);
+  }
+  $('.responsibilities-entry:last').append("<br>");
+ }
+}
+
+displayResponsibility();
+
+
+// to know the coordinates of mouse when clicked!
+$(document).click(function(loc) {
+ var x = loc.pageX;
+ var y = loc.pageY;
+
+ logClicks(x, y);
+})
+
+// Adding map!
+$("#mapDiv").append(googleMap);
